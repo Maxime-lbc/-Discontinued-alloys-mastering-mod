e17a62616e3b59fc24f93cc4944e001b5bf90391
Maxime LE BONNIEC <maxime.lbc.pro@gmail.com>
2021-03-29 01:10:18 +0200

    Carbon chunk item implementation

---
+++ src/main/java/fr/lebonniec/alloysmastering/init/ModItems.java
@@ -0,0 +1,20 @@
+package fr.lebonniec.alloysmastering.init;
+
+import fr.lebonniec.alloysmastering.utils.References;
+import net.minecraft.item.Item;
+import net.minecraftforge.fml.RegistryObject;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+
+public class ModItems
+{
+
+    public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, References.MODID);
+
+    /* Carbon */
+    public static final RegistryObject<Item> CARBON_CHUNK = ITEMS.register(
+            "carbon_chunk",
+            () -> new Item(new Item.Properties()
+                    .maxStackSize(64)
+            ));
+}
